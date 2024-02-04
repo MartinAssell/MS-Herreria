8f980336f7af4b10a12b8cbee5293726d42c2e24
Martin <martinassell@gmail.com>
2024-02-03 16:36:08 -1200

    Correo andando

--- src/routes/contactRoutes.js
+++ src/routes/contactRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const contactControllers = require("../controllers/contactControllers");
-
-router.post('/contacts', contactControllers.processMessage)
+const nodemailer = require('nodemailer');
 router.get("/contact", contactControllers.home)
+router.post("/contacts", express.urlencoded({ extended: true }), contactControllers.processMessage);
+
 
 module.exports = router;
+
