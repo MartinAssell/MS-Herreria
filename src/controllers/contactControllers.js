4749155728337e9c684d92e4ab9db32c2aa63452
Martin <martinassell@gmail.com>
2024-02-03 15:19:17 -1200

    mejoras en env y envío de email

--- src/controllers/contactControllers.js
+++ src/controllers/contactControllers.js
@@ -1,8 +1,39 @@
 const path = require("path");
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
 const contactController = {
     home: (req, res) =>{
         res.render(path.join(__dirname,"../views/contact"))
-    }
+    },
+    processMessage: (req, res) => {
+        const { name, message, phone, email } = req.body;
+        console.log('Datos del formulario:', { name, message, phone, email });
+      
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+              user: process.env.GMAIL_USER, 
+              pass: process.env.GMAIL_PASS
+          }
+        });
+      
+        const mailOptions = {
+            from: email,
+            to: 'martinassell@gmail.com',
+            subject: 'MARIANO SAENZ METALURGICA CONTACTO',
+            text: `Nombre: ${name}\nTeléfono: ${phone}\nEmail: ${email}\n\nMensaje:\n${message}`
+        };
+      
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.error(error);
+            }
+            console.log('Correo electrónico enviado:', info.response);
+        });
+      
+        res.send('mensaje enviado exitosamente!');
+      }
+
 }
 
 module.exports = contactController;
--- src/routes/contactRoutes.js
+++ src/routes/contactRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const contactControllers = require("../controllers/contactControllers");
+
+router.post('/contacts', contactControllers.processMessage)
 router.get("/contact", contactControllers.home)
 
 module.exports = router;
