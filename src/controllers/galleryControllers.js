b20bb83e05178e86768a48cf0c480ffdc6d9cde0
Martin <martinassell@gmail.com>
2024-02-17 20:13:28 -1200

    subida de imagenes en crudo a la vista de venta de productos

--- src/controllers/galleryControllers.js
+++ src/controllers/galleryControllers.js
@@ -2,19 +2,17 @@ const Productos = require('../database/models');
 const db = require("../database/models");
 
 const path = require('path');
-
 const galleryController = {
-    home: async (req, res) => {
-        try {
-            // Recupera todos los productos de la base de datos
-            const productos = await db.Productos.findAll();
-
-            // Renderiza la vista y pasa los productos a la misma
-            res.render(path.join(__dirname, '../views/gallery'), { productos: productos });
-        } catch (error) {
-            console.error('Error al consultar la base de datos:', error);
-            res.status(500).send('Error interno del servidor');
-        }
+    home: function(req, res) {
+        db.Productos.findAll()
+            .then(function(productos){
+                const ruta = path.join(__dirname, '../views/gallery');
+                res.render(ruta, { productos: productos });
+            })
+            .catch(function(error) {
+                console.error('Error al consultar la base de datos:', error);
+                res.status(500).send('Error interno del servidor');
+            });
     }
 };
 
